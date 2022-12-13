ecc9811a6e662dc07a6be7caa7e174eb2af42b58
Ashley Arena <116913112+aecarena@users.noreply.github.com>
2022-12-12 21:18:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+//Define variables
+var saveBtn = $ (".saveBtn")
+var today= $ (newdate());
+var newdate= today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var Time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+console.log(newdateTime)
+
+var moment = $ (newdateTime)
+// Display today's day and date
+//var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+//$("#currentDay").html(todayDate);
+//tells computer to load DOM first
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).child(".description").val();
+        var time = $(this).parent().attr("id");
+        console.log(this);
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+
+
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("-future");
+                $(this).removeClass("-present");
+                $(this).addClass("-past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("-past");
+                $(this).removeClass("-future");
+                $(this).addClass("-present");
+            }
+            else if (blockTime > timeNow) {
+                $(this).removeClass("-present");
+                $(this).removeClass("-past");
+                $(this).addClass("f-uture");
+
+            }
+        })
+    }
+    // Get item from local storage if any
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+   
+
+    timeTracker();
+})
